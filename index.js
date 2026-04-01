de871a9ce4498b03f68a34ccc539bf77e439cf6a
Jenil1105 <unselfstudy@gmail.com>
2026-04-02 01:28:35 +0530

    Add user registration functionality with validation

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRouter)
 
 mongoose.connect(MONGO_URI)
     .then(()=>{
